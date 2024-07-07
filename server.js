b79a5c98d1efa80d0ba70b6bc051f241cb02c186
olesyamaniuk <maniukolesya222@gmail.com>
2024-07-07 16:08:36 +0300

    1

---
+++ server.js
@@ -0,0 +1,39 @@
+// const express = require('express');
+// const axios = require('axios');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/api/search', async (req, res) => {
+//     const { query } = req.query;
+//     try {
+//         const response = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
+//         res.json(response.data);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/search', async (req, res) => {
+    const { query } = req.query;
+    try {
+        const response = await axios.get(`https://api.github.com/search/repositories?q=${query}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
